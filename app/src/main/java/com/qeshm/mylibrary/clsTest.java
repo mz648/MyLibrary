c90e775d2ed1cd8218090c075c79f7d61ec05f0e
Mahmoud Zivari <zivari.ts@gmail.com>
2018-10-05 21:20:51 +0330

    commit3

---
+++ app/src/main/java/com/qeshm/mylibrary/clsTest.java
@@ -0,0 +1,10 @@
+package com.qeshm.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class clsTest {
+    public static void test(Context context){
+        Toast.makeText(context,"hiiii",Toast.LENGTH_LONG).show();
+    }
+}
