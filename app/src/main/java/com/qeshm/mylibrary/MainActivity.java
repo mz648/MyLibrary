ad50e4509878b0b765b11ee803903316dca256af
Mahmoud Zivari <zivari.ts@gmail.com>
2018-10-05 20:54:43 +0330

    Initial commit

---
+++ app/src/main/java/com/qeshm/mylibrary/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qeshm.mylibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.qeshm.mylib.*;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
